80303d9d6b8e28c029270c246fc3db22104a2e2a
therealozp <anhkhang.le0910@gmail.com>
2024-08-21 17:46:59 -0700

    hotfix for stripe

--- src/routes/create-payment-intent/+server.js
+++ src/routes/create-payment-intent/+server.js
@@ -3,17 +3,15 @@ import { SECRET_STRIPE_KEY } from '$env/static/private';
 
 // Initialize Stripe
 const stripe = new Stripe(SECRET_STRIPE_KEY);
-
 export async function POST() {
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 2000,
-    currency: 'usd',
-    payment_method_types: ['card']
-  });
+	// console.log(SECRET_STRIPE_KEY);
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: 2000,
+		currency: 'usd',
+		payment_method_types: ['card']
+	});
 
-  return {
-    body: {
-      clientSecret: paymentIntent.client_secret
-    }
-  };
+	return new Response(JSON.stringify({ client_secret: paymentIntent.client_secret }), {
+		status: 200
+	});
 }
