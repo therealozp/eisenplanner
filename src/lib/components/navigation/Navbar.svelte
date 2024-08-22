566a242ba285125fe878630c6c0806dfb2beb2f5
Swan Thurein <thureinswanhtet7@gmail.com>
2024-08-21 20:07:14 -0400

    Revert "Stuck at getting the backend work"
    This reverts commit ae5bc12f1d4c1b09a149c2821df7a5458c7eb840.

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -9,12 +9,11 @@
 			<img src="/eisen.svg" alt="EisenPlanner" class="h-10" />
 		</div>
 		<div class="flex items-center">
-			<Button variant="outlined" class="mr-4"><a href="/">Home</a></Button>
-			<Button variant="outlined" class="mr-4"><a href="/about">About</a></Button>
-			<Button variant="outlined" class="mr-4"><a href="/contact">Contact</a></Button>
-			<Button variant="outlined" class="mr-4"><a href="/pricing">Pricing</a></Button>
-			<Button variant="outlined" class="mr-4"><a href="/login">Login</a></Button>
-			<Button variant="contained"><a href="/signup">Sign Up</a></Button>
+			<Button variant="outlined" class="mr-4">Home</Button>
+			<Button variant="outlined" class="mr-4">About</Button>
+			<Button variant="outlined" class="mr-4">Contact</Button>
+			<Button variant="outlined" class="mr-4">Login</Button>
+			<Button variant="contained">Sign Up</Button>
 		</div>
 		<div>
 			<User size="24" />
